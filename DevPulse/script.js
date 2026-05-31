0a410e194da1c9f32c4168490fd0d9089533b902
Purno <purnorunjunshqul1@gmail.com>
2026-05-31 14:17:48 +0600

    Name Changes

--- DevPulse/script.js
+++
@@ -1,9 +0,0 @@
-let prices = [100,200,300,400,500,600];
-let dis;
-for (let i =0; i<=prices.length; i++)
-{
-    dis = prices[i]*0.10;
-    console.log(`Discount Price of ${prices[i]} is prices ${dis}`)
-}
-
-console.log(prices)
